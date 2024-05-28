bd0464ab05850012bf550c90a4a0746d83301939
Parthebhan Pari <152586277+parthebhan@users.noreply.github.com>
2024-05-28 13:53:32 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from bardapi import Bard
+import streamlit as st
+import os
+
+os.environ['_BARD_API_KEY'] = "AIzaSyCOW5_2uNp7RxnLhLmGKRFhx5WAROj81rs"
+
+st.title("Google Bard Clone Chatbot")
+
+def response_api(prompt):
+ 
+        message = Bard().get_answer(str(prompt))['content']
+        return message
+
+def user_input():
+        input_text = st.text_input("Enter Your prompt: ")
+        return input_text
+
+if 'generate' not in st.session_state:
+    st.session_state['generate']=[]
+if 'past' not in st.session_state:
+    st.session_state['past']=[]
+
+user_text = user_input()
+
+if user_text:
+  output = response_api(user_text)
+  st.session_state.past.append(user_text)
+  st.session_state.generate.append(output)
+
+if st.session_state['generate']:
+
+  for i in range(len(st.session_state['generate'])-1,-1,-1):
+    message(st.session_state['past'][i], is_user=True,key=str(i) + '_user')
+    message (st.session_state['generate'][i], key=str(i))
