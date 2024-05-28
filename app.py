cf1671fc81a80e8d9b566a47b58e0113523e1bb3
Parthebhan Pari <152586277+parthebhan@users.noreply.github.com>
2024-05-28 17:44:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import google.generativeai as genai
+
+# Configure the API key
+api_key = st.secrets["auth_token"]
+genai.configure(api_key=api_key)
+
+# Function to load Gemini Pro model and get responses
+model = genai.GenerativeModel("gemini-1.5-pro-latest")
+chat = model.start_chat(history=[])
+
+def get_gemini_response(question):
+    response = chat.send_message(question, stream=False)
+    return response.text
+
+# Initialize Streamlit app
+st.set_page_config(page_title="Q&A Demo")
+
+st.header("Gemini Clone - Text-Based Chatbot")
+st.write("")
+st.write("Done by: Parthebhan Pari")
+st.write("")
+st.write("Enter your query below and click 'Submit' to get a response:")
+
+# Initialize session state for chat history if it doesn't exist
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
+
+input_text = st.text_input("Input: ", key="input")
+submit_button = st.button("Submit!")
+
+if submit_button and input_text:
+    response = get_gemini_response(input_text)
+    # Add user query and response to session state chat history
+    st.session_state['chat_history'].append(("You", input_text))
+    st.subheader("The Response is")
+    st.write(response)
+    st.session_state['chat_history'].append(("Gemini", response))
+
+st.subheader("The Chat History is")
+for role, text in st.session_state['chat_history']:
+    st.write(f"{role}: {text}")
